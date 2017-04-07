de12a3aa1daeb1c14f4a84d2ece170ddc2c930a6
Feng Zhao <fezhao@ean.com>
2017-04-07 14:59:54 +0100

    feature gen

--- feature_gen.py
+++ feature_gen.py
@@ -11,8 +11,8 @@ import numpy as np
 def generate_spatial_agg_features(X, input_shape=(11, 11, 11, 256)):
     img_input = Input(shape=input_shape)
 
-    x = MaxPooling3D((3, 3, 3), strides=(3, 3, 3), name='block1_pool')(img_input)
-    # x = AveragePooling3D((3, 3, 3), strides=(3, 3, 3), name='block1_pool')(img_input)
+    x = MaxPooling3D((3, 3, 3), strides=(3, 3, 3), name='block1_pool', padding='same')(img_input)
+    # x = AveragePooling3D((3, 3, 3), strides=(3, 3, 3), name='block1_pool', padding='same')(img_input)
     x = Flatten(name='flatten')(x)
 
     model = Model(inputs=img_input, outputs=x)
