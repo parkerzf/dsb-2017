ec18d5dbd21a681f414251c518fc5c6acc60eddd
Feng Zhao <fezhao@ean.com>
2017-04-08 17:51:50 +0100

    add train/test xgboost model

--- feature_gen.py
+++ feature_gen.py
@@ -6,27 +6,51 @@ from keras.models import Model
 from keras import backend as K
 
 import numpy as np
+import pandas as pd
+
+from sklearn.externals import joblib
 
 
 def generate_spatial_agg_features(X, input_shape=(11, 11, 11, 256)):
     img_input = Input(shape=input_shape)
 
-    x = MaxPooling3D((3, 3, 3), strides=(3, 3, 3), name='block1_pool', padding='same')(img_input)
-    # x = AveragePooling3D((3, 3, 3), strides=(3, 3, 3), name='block1_pool', padding='same')(img_input)
+    # x = MaxPooling3D((4, 4, 4), strides=(4, 4, 4), name='block1_pool', padding='same')(img_input)
+    x = AveragePooling3D((3, 3, 3), strides=(3, 3, 3), name='block1_pool', padding='same')(img_input)
     x = Flatten(name='flatten')(x)
 
     model = Model(inputs=img_input, outputs=x)
 
-    res = model.predict(X)
-    print res.shape
-
+    return model.predict(X)
 
 K.set_image_data_format('channels_last')
 
-X = np.load('0121c2845f2b7df060945b072b2515d7.npy')
-X = X.reshape(1, 11, 11, 11, 256)
+df_train = pd.read_csv('data/stage1_labels.csv')
+df_val = pd.read_csv('data/stage1_solution.csv')
 
-generate_spatial_agg_features(X)
+train_chunks = df_train.id.apply(lambda x: np.load('feature/%s.npy' % str(x)))
+
+train_chunks = np.dstack(train_chunks)
+train_chunks = np.rollaxis(train_chunks, -1).reshape(train_chunks.shape[2], 11, 11, 11, 256)
+X_train = generate_spatial_agg_features(train_chunks)
+y_train = df_train.cancer.astype(int)
+
+joblib.dump(X_train, 'data/X_train.pkl')
+joblib.dump(y_train, 'data/y_train.pkl')
+
+val_chunks = df_val.id.apply(lambda x: np.load('feature/%s.npy' % str(x)))
+
+val_chunks = np.dstack(val_chunks)
+val_chunks = np.rollaxis(val_chunks, -1).reshape(val_chunks.shape[2], 11, 11, 11, 256)
+X_val = generate_spatial_agg_features(val_chunks)
+y_val = df_val.cancer.astype(int)
+
+joblib.dump(X_val, 'data/X_val.pkl')
+joblib.dump(y_val, 'data/y_val.pkl')
+
+
+
+
+
 
 
 
